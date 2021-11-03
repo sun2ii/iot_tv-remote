3b40b2eb6f87e98b47a2aed27211975a13c3ddc2
CODE <dhruv.n.rajpurohit@gmail.com>
2021-11-03 15:23:11 +0530

    Updating files to add new features
    Home, Info, Mute, Settings, Channel Up-Down, Previous Channel, Input Button Added

--- vizio-remote/GlobalVar.swift
+++ vizio-remote/GlobalVar.swift
@@ -3,15 +3,20 @@ import Foundation
 public struct GlobalVar {
     static var HOST = "10.0.1.18"
     static var AUTH_TOKEN = "Z5ygsy6skc"
-    struct CMD {
-        static var TEST = [3, 8]
-
+     struct CMD {
+        
+        //static var TEST = [11, 2]
+        
         struct MAIN {
             static var POWER = [11, 2]
             static var MUTE = [5, 4]
             static var ENTER = [3, 2]
-            static var EXIT = [9, 0]
+            static var Settings = [4, 8]
+            static var INFO = [4,6]
+            static var HOME = [4,3]
             static var BACK = [4, 0]
+            static var PREV = [8, 2]
+            static var INPUT = [7, 1]
         }
         
         struct BASIC {
@@ -32,7 +37,7 @@ public struct GlobalVar {
             static var NETFLIX = [nil, 3, "1"] as! Array<Any>
             static var PRIME = [nil, 2, "4"] as! Array<Any>
             static var YOUTUBE = [nil, 5, "1"] as! Array<Any>
-            static var DISNEY = [] as Any
+//            static var DISNEY = [] as Any
         }
     }
 }
